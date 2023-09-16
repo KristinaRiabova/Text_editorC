668af332dc1751f1884725a945127b4ba5411763
KristinaRiabova <kristinaer304@gmail.com>
2023-09-16 18:56:34 +0300

    Structure to represent text memory storage, structure to represent a line of text, Function to initialize a new TextStorage structure

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+typedef struct {
+    char *text;
+    size_t length;
+} Line;
+
+
+typedef struct {
+    Line *lines;
+    size_t numLines;
+} TextStorage;
+
+
+
+
 void printHelp() {
     printf("Choose the command:\n");
     printf("1. Append text symbols to the end\n");
@@ -12,27 +26,31 @@ void printHelp() {
     printf("0. Exit\n");
 }
 
+
+TextStorage initializeTextStorage() {
+    TextStorage storage;
+    storage.lines = NULL;
+    storage.numLines = 0;
+    return storage;
+}
+
 int main() {
     int choice;
+    TextStorage storage = initializeTextStorage();
+    char inputBuffer[256];
+
     do {
         printHelp();
         printf("Choose the command: ");
         scanf("%d", &choice);
         getchar();
-
         switch (choice) {
             case 0:
                 printf("Exiting the text editor.\n");
                 break;
-            default:
                 printf("The command is not implemented.\n");
         }
     } while (choice != 0);
 
     return 0;
 }
-
-
-
-
-
