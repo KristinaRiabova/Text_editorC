d126ee10507c4bb2c8fe1f9e02a61b7f502ef1a4
KristinaRiabova <kristinaer304@gmail.com>
2023-09-16 19:11:55 +0300

    Function to free memory used by TextStorage.Function to append text to the end of the current line

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct {
     char *text;
@@ -34,6 +36,33 @@ TextStorage initializeTextStorage() {
     return storage;
 }
 
+
+void freeTextStorage(TextStorage *storage) {
+    for (size_t i = 0; i < storage->numLines; i++) {
+        free(storage->lines[i].text);
+    }
+    free(storage->lines);
+}
+
+void appendText(TextStorage *storage, const char *text) {
+    size_t newLength = strlen(text);
+    size_t currentLineIndex = storage->numLines - 1;
+
+    if (storage->lines == NULL) {
+        // Initialize the first line
+        storage->lines = (Line *)malloc(sizeof(Line));
+        storage->numLines = 1;
+        storage->lines[0].text = (char *)malloc(newLength + 1);
+        strcpy(storage->lines[0].text, text);
+        storage->lines[0].length = newLength;
+    } else if (currentLineIndex < storage->numLines) {
+        size_t currentLength = storage->lines[currentLineIndex].length;
+        storage->lines[currentLineIndex].text = (char *)realloc(storage->lines[currentLineIndex].text, currentLength + newLength + 1);
+        strcat(storage->lines[currentLineIndex].text, text);
+        storage->lines[currentLineIndex].length += newLength;
+    }
+}
+
 int main() {
     int choice;
     TextStorage storage = initializeTextStorage();
@@ -44,11 +73,20 @@ int main() {
         printf("Choose the command: ");
         scanf("%d", &choice);
         getchar();
+
         switch (choice) {
             case 0:
                 printf("Exiting the text editor.\n");
+                freeTextStorage(&storage);
                 break;
-                printf("The command is not implemented.\n");
+            case 1:
+                printf("Enter text to append: ");
+                fgets(inputBuffer, sizeof(inputBuffer), stdin);
+                inputBuffer[strcspn(inputBuffer, "\n")] = '\0';
+                appendText(&storage, inputBuffer);
+                break;
+                default:
+                    printf("The command is not implemented.\n");
         }
     } while (choice != 0);
 
